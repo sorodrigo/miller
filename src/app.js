6c160d63c737eb72c9039f5492b0438940527312
Rodrigo Solis <sorodrigo@users.noreply.github.com>
2016-12-26 02:56:18 +0100

    adds more styles and fixes issue with rows being inserted in wrong columns

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ document.onreadystatechange = () => {
     /* eslint-disable no-new */
     const set = new Set([4, 6, 9999999]);
     new Miller('#miller', {
-      data: ['one', 'two', 'three', { parent: 'cuatro', children: [1, { parent: 2, children: [2.2, { parent: 2.3, children: set }, 2.7, 2.9] }, 3] }]
+      data: ['one', 'two', 'three','one', 'three','one', 'two', 'three','one', 'two', 'three', { parent: 'cuatro', children: [1, { parent: 'list', children: ['two', 'three','one', 'two', 'three','one', 'two', 'three','one', 'two', { parent: 2, children: [2.2, { parent: 2.3, children: set },] }, 2.7, 2.9] }, 3] }],
+      minColumns: 2
     });
   }
 };
--- src/miller.js
+++ src/miller.js
@@ -5,6 +5,7 @@ const defaults = {
   millerClass: 'miller',
   columnClass: 'column',
   rowClass: 'row',
+  parentClass: '-parent',
   rows: new Map(),
   columns: [],
 };
@@ -23,7 +24,7 @@ class Miller {
   }
 
   static isEmptyColumn(column) {
-    return !!column.children.length;
+    return column && !column.children.length;
   }
 
   static createOption(row, id, className) {
@@ -49,8 +50,10 @@ class Miller {
     select.setAttribute('id', `column-${this.options.column}`);
     select.setAttribute('size', this.options.columnSize);
     select.setAttribute('class', this.options.columnClass);
+    select.setAttribute('tabindex', this.options.column);
     select.addEventListener('change', this.selectRow.bind(this));
     this.el.appendChild(select);
+    select.scrollIntoView();
     this.options.column += 1;
     return select;
   }
@@ -64,11 +67,13 @@ class Miller {
 
   addRow(row, select) {
     if (Miller.isLeaf(row)) {
-      const option = Miller.createOption(row, `${select.id}-row-${select.children.length}`, this.options.rowClass);
+      const style = this.options.rowClass;
+      const option = Miller.createOption(row, `${select.id}-row-${select.children.length}`, style);
       select.appendChild(option);
       this.options.rows.set(option.id, row);
     } else if (typeof row === 'object') {
-      const option = Miller.createOption(row.parent, `${select.id}-row-${select.children.length}`, this.options.rowClass);
+      const style = `${this.options.rowClass} ${this.options.parentClass}`;
+      const option = Miller.createOption(row.parent, `${select.id}-row-${select.children.length}`, style);
       select.appendChild(option);
       this.options.rows.set(option.id, row);
     }
@@ -92,13 +97,13 @@ class Miller {
   updateColumns(row, columnID) {
     if (Miller.isLeaf(row)) {
       this.removeColumns(this.options.columns.splice(columnID + 1));
-      if (this.options.columns.length < this.options.minColumns) {
+      while (this.options.columns.length < this.options.minColumns) {
         this.options.columns.push(this.addColumn());
       }
     } else if (typeof row === 'object') {
-      const last = this.options.columns.length - 1;
-      if (!Miller.isEmptyColumn(this.options.columns[last])) {
-        this.insertRows(row.children, this.options.columns[last]);
+      const next = columnID + 1;
+      if (Miller.isEmptyColumn(this.options.columns[next])) {
+        this.insertRows(row.children, this.options.columns[next]);
       } else {
         this.removeColumns(this.options.columns.splice(columnID + 1));
         const column = this.addColumn();
