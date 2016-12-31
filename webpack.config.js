1f1b38005c2050abf63a1ac6042c6b69f50cebbe
Rodrigo Solis <sorodrigo@users.noreply.github.com>
2016-12-31 14:59:52 +0100

    includes webpack plugins and fixes npm run build command

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
+var UglifyJsPlugin = require('webpack-uglify-js-plugin');
 
 var cssnext = require('postcss-cssnext');
 var nested = require('postcss-nested');
@@ -10,16 +11,42 @@ var wordwrap = require('wordwrap');
 
 var colors = require('colors');
 
-let sassExtract = new ExtractTextPlugin('styles/app.css');
+let sassExtract = new ExtractTextPlugin('styles/miller.css');
+let webpackPlugins = [
+  sassExtract,
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new CopyWebpackPlugin([
+    {
+      context: 'src',
+      from: '**/*.html',
+      to: '.'
+    }
+  ])
+];
+
+if (process.env.NODE_ENV === 'production') {
+  webpackPlugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      dead_code: true,
+      drop_debugger: true,
+      drop_console: true
+    },
+    comments: false
+  }));
+}
 
 module.exports = {
   entry: {
-    app: ['./src/app.js']
+    app: ['./src/app.js', 'webpack-hot-middleware/client'],
+    miller: ['./src/miller.js', 'webpack-hot-middleware/client']
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     preLoaders: [
@@ -49,14 +76,5 @@ module.exports = {
   eslint: {
     formatter: require('eslint-friendly-formatter')
   },
-  plugins: [
-    sassExtract,
-    new CopyWebpackPlugin([
-      {
-        context: 'src',
-        from: '**/*.html',
-        to: '.'
-      }
-    ])
-  ],
+  plugins: webpackPlugins,
 };
